242f7e43b4e372ce87acfc0f1667748e6424b52b
Kevin Mario Gerard <kevinmariogerard@gmail.com>
2019-10-25 11:42:52 +0530

    Reversal Algorithm for Array Rotation

--- src/main/java/com/letsgoalgo/problems/arrays/ArrayRotation.java
+++ src/main/java/com/letsgoalgo/problems/arrays/ArrayRotation.java
@@ -2,7 +2,7 @@ package com.letsgoalgo.problems.arrays;
 
 import java.util.Arrays;
 
-public class ArrayReversal {
+public class ArrayRotation {
 
     /*
     Write a function rotate(arr[], d, n) that rotates arr[] of size n by d elements.
@@ -13,7 +13,7 @@ Input :  arr[] = [1, 2, 3, 4, 5, 6, 7]
 Output : arr[] = [3, 4, 5, 6, 7, 1, 2]
      */
 
-    public static int[] rotate(int[] input,int k){
+    public static int[] rotate(int[] input, int k){
         int[] rotated = new int[input.length];
         for(int i = 0 ; i < input.length ; i++){
             // Java's % will not work here
@@ -24,6 +24,23 @@ Output : arr[] = [3, 4, 5, 6, 7, 1, 2]
         return rotated;
     }
 
+    public static void reverse(int[] arr, int start, int end) {
+        while(start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    // Time Complexity: O(n)
+    public static void reversalAlgo(int[] input, int k) {
+        reverse(input, 0, k - 1);
+        reverse(input, k, input.length - 1);
+        reverse(input, 0, input.length - 1);
+    }
+
     public static void printArray(int[] input){
         for(int i = 0 ; i < input.length ; i++){
             System.out.print(input[i] + " ");
@@ -32,9 +49,13 @@ Output : arr[] = [3, 4, 5, 6, 7, 1, 2]
     }
 
     public static void main(String[] args) {
-        int[] input = new int[]{1, 2, 3, 4, 5, 6, 7};
-        int[] rotated = rotate(input,2);
+        int[] input1 = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int[] rotated = rotate(input1, 2);
         printArray(rotated);
+
+        int[] input2 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        reversalAlgo(input2, 4);
+        printArray(input2);
     }
 
 }
