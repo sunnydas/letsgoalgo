f2c8685dee1e3bf5dd9b1801c759a68a57207cfb
Sunny Das <das.sunny@gmail.com>
2019-11-12 18:46:36 +0530

    Adding print logic for dp.

--- src/main/java/com/letsgoalgo/problems/arrays/strings/EditDistanceDP.java
+++ src/main/java/com/letsgoalgo/problems/arrays/strings/EditDistanceDP.java
@@ -25,7 +25,18 @@ public class EditDistanceDP {
                }
            }
        }
+       printMatrix(dp);
       return dp[word1.length()][word2.length()];
+    }
+
+    public static void printMatrix(int[][] dp){
+        for(int i = 0 ; i < dp.length ; i++){
+            for(int j = 0 ; j < dp[i].length ; j++){
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
